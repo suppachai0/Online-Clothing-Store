00ff5c7afcb4f9bf97374d429a6816503e0fd4e7
Suppachai Wicheer <suppachai4454@gmail.com>
2025-02-08 13:54:47 +0700

    profile

--- clothing_store/urls.py
+++ clothing_store/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from .views import index, product_list, login_view, register_view, dashboard_view, profile_view, logout_view
 
 urlpatterns = [
-    path('products/', views.product_list, name='product_list'),
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('products/', product_list, name='product_list'),
+    path('login/', login_view, name='login'),
+    path('signup/', register_view, name='signup'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('profile/', profile_view, name='profile'),
+    path('logout/', logout_view, name='logout'),
 ]
--- clothing_store/views.py
+++ clothing_store/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
-from .models import Product
-
-def product_list(request):
-    products = Product.objects.all()  # ดึงข้อมูลสินค้าทั้งหมด
-    return render(request, 'clothing_store/product_list.html', {'products': products})
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
-    return render(request, 'clothing_store/index.html')
-# Create your views here.
+    return render(request, "clothing_store/index.html")
+
+def product_list(request):
+    return render(request, "clothing_store/product_list.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            messages.error(request, "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง")
+    return render(request, "clothing_store/login.html")
+
+def register_view(request):
+    return render(request, "clothing_store/register.html")
+
+def dashboard_view(request):
+    return render(request, "clothing_store/dashboard.html")
+
+def profile_view(request):
+    return render(request, "clothing_store/profile.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
